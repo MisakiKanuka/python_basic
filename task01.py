ac17d3ef64cbafc875291d06d33e4ca5180283df
Kanuka <kanukamisaki@icloud.com>
2018-11-21 20:23:04 +0900

    kazuma_infoリストを定義

--- task01.py
+++ task01.py
@@ -1,3 +1,9 @@
-users = ['Bob', 'Tom', 'Ken']
+users = ["Bob", "Tom", "Ken"]
+
+print(users)
 
 int_numbers = [1, 2, 3, 4, 5]
+print(int_numbers)
+
+kazuma_info = ["Kazuma", "Takahashi", 35]
+print(kazuma_info)
