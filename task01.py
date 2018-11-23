6a631f1e8e98c77aee76a7f756e2fd8780fc6b68
Kanuka <kanukamisaki@icloud.com>
2018-11-23 16:10:59 +0900

    辞書を定義 意外と覚えてた！よっしゃ！

--- task01.py
+++ task01.py
@@ -12,18 +12,23 @@ print(members[0], members[2])
 
 print("Name:" + kazuma_info[1] + kazuma_info[0] + ",Age:" + str(kazuma_info[2]))  #A-5
 
-odd_numbers = [1, 3, 7, 9]  # A-6
+odd_numbers = [1, 3, 7, 9]  #A-6
 for number_1 in odd_numbers:
     print(number_1)
 
-even_numbers = [2, 4, 6, 8]  # A-7
+even_numbers = [2, 4, 6, 8]  #A-7
 for numbers_2 in even_numbers:
     print(numbers_2 * 2)
 
-users_info = [["Kazuma", 35], ["Tom", 57], ["Bob", 77]]  # A-8
+users_info = [["Kazuma", 35], ["Tom", 57], ["Bob", 77]]  #A-8
 for users_list in users_info:
     print("Name: " + users_list[0] + ", Age: " + str(users_list[1]))
 # こうなれば良い
 # "Name: Kzuma, Age: 35"
 # "Name: Tom, Age: 57"
 # "Name: Bob, Age: 77"
+
+kazuma_info = {"first_name": "Kazuma", "family_name": "Takahashi", "age": 35}  # A-9
+print(kazuma_info["first_name"])  # "Kazuma"
+print(kazuma_info["family_name"])  # "Takahashi"
+print(kazuma_info["age"])  # 35
