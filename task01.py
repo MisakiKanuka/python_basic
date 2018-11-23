4c4a4a93814801c8adcd7cf893cf9fbfd8f59f51
Kanuka <kanukamisaki@icloud.com>
2018-11-23 13:37:29 +0900

    kazuma_infoリストを利用して、 Name:TakahashiKazuma,Age:35を表示 もっとスマートにできる気がする...

--- task01.py
+++ task01.py
@@ -1,12 +1,13 @@
-users = ["Bob", "Tom", "Ken"]
-
+users = ["Bob", "Tom", "Ken"]  # A-1
 print(users)
 
-int_numbers = [1, 2, 3, 4, 5]
+int_numbers = [1, 2, 3, 4, 5]  #A-2
 print(int_numbers)
 
-kazuma_info = ["Kazuma", "Takahashi", 35]
+kazuma_info = ["Kazuma", "Takahashi", 35]  #A-3
 print(kazuma_info)
 
-members = ["Makoto", "Kazu", "Kazuma"]
+members = ["Makoto", "Kazu", "Kazuma"]  #A-4
 print(members[0], members[2])
+
+print("Name:" + kazuma_info[1] + kazuma_info[0] + ",Age:" + str(kazuma_info[2]))  #A-5
